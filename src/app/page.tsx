777787cf410668bf4ea408f2d4fc474f6de7639b
Prashu11 <jainprashu11@gmail.com>
2024-09-20 06:31:50 -0700

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <strong>Test page for AWS Code Pipeline Setup</strong>
+    </div>
+  );
+}
